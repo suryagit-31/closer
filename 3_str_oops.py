900e8a144b55b39db49ad617c799fca55310be91
suryagit-31 <153883780+suryagit-31@users.noreply.github.com>
2023-12-15 09:48:52 +0530

    Add files via upload

---
+++ 3_str_oops.py
@@ -0,0 +1,20 @@
+class bigone:  
+    
+    def __init__(self,name,age):
+        self.drem=name #instance varirable is created drem and kola
+        self.kola=age
+    def __str__(KIN):
+        return f"we have name {KIN.drem}"
+        
+    def make(self,glue,age):
+            print(f"he is good boy{glue} and {age}")
+
+obj1=bigone("coke",78)
+obj2=bigone("sprite",99)
+obj1.make(" teja",22)
+ 
+
+
+print(obj1)#invokes the str
+print(obj2)
+
